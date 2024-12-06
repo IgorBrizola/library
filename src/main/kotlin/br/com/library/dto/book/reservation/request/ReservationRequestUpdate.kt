a8e4f13542bd4877010d1e75f8e89f372141bb72
IgorBrizola <igorbrizzola@gmail.com>
2024-12-06 17:11:43 -0300

    chore: delete class sem uso

--- src/main/kotlin/br/com/library/dto/book/reservation/request/ReservationRequestUpdate.kt
+++
@@ -1,7 +0,0 @@
-package br.com.library.dto.book.reservation.request
-
-import br.com.library.model.book.reservation.Reservation
-
-data class ReservationRequestUpdate(
-    val status: Reservation.ReservationStatus,
-)
