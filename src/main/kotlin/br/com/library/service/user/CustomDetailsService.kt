5c42e5c92c554b3223d3c32221946f71d24bed8c
IgorBrizola <igorbrizzola@gmail.com>
2024-12-13 13:34:37 -0300

    feat: add custom service for transform user in userDetails

---
+++ src/main/kotlin/br/com/library/service/user/CustomDetailsService.kt
@@ -0,0 +1,26 @@
+package br.com.library.service.user
+
+import br.com.library.repository.user.UserRepository
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class CustomDetailsService(
+    private val userRepository: UserRepository
+): UserDetailsService {
+
+    override fun loadUserByUsername(username: String): UserDetails {
+
+        val user =  userRepository.findByEmailAndActiveIsTrue(username)
+            ?: throw UsernameNotFoundException("user with email $username not found!")
+
+        return User.builder()
+                .username(user.email)
+                .password(user.password)
+                .roles(user.roles.name)
+                .build()
+    }
+}
